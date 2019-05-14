089fae757f45d76b0a1a5d7833101a57f8294ecb
vebradev <justinas@vebra.dev>
2019-05-15 00:28:31 +0300

    Added mouseover/mouseout event listeners.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+
+const header = document.querySelector("header");
+const container = document.querySelector(".home");
+
+// mouseover
+
+header.addEventListener('mouseover', () => { 
+    header.classList.add("shadow");
+    container.classList.add("opacity");
+});
+
+header.addEventListener('mouseout', () => { 
+    header.classList.remove("shadow");
+    container.classList.remove("opacity");
+});
+
+// keydown
+
+
+// wheel
+// drag / drop
+// load
+// focus
+// resize
+// scroll
+// select
+// dblclick
