08cf9ab2bba974f37eaa9c1e0124cfe416ba5ad0
vebradev <justinas@vebra.dev>
2019-05-14 15:22:07 +0300

    Added click, mouseover & dblclick event listeners.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+
+// Event listeners
+
+const nav = document.querySelector(".nav");
+nav.addEventListener('click', () => nav.classList.add("rotate-center"));
+
+// mouseover
+const headerImg = document.querySelector(".intro img");
+headerImg.addEventListener('mouseover', () => headerImg.classList.add("rotate-center"));
+
+// keydown
+// wheel
+// drag / drop
+// load
+// focus
+// resize
+// scroll
+// select
+
+// dblclick
+const header = document.querySelector("header");
+header.addEventListener('dblclick', () => {
+    let a = Math.floor(Math.random() * 100);
+    let b = "red";
+    header.style.backgroundColor = `#DD${a}${a}`;
+});
