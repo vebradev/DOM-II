c06af87c492b10e040b0ce7c2bebf86b759b0c3b
vebradev <justinas@vebra.dev>
2019-05-14 17:04:40 +0300

    Added wheel, load & resize event listeners.

--- js/index.js
+++ js/index.js
@@ -3,25 +3,51 @@
 // Event listeners
 
 const nav = document.querySelector(".nav");
-nav.addEventListener('click', () => nav.classList.add("rotate-center"));
+nav.addEventListener("click", () => nav.classList.add("rotate-center"));
 
 // mouseover
-const headerImg = document.querySelector(".intro img");
-headerImg.addEventListener('mouseover', () => headerImg.classList.add("rotate-center"));
+const img = document.querySelectorAll("img");
 
-// keydown
+function addClass(element) {
+    element.target.classList.add("rotate-center");
+}
+
+img.forEach((item) => {
+    item.addEventListener("mouseover", addClass);
+});
+
+
 // wheel
-// drag / drop
+const body = document.querySelector("body");
+body.addEventListener("wheel", () => {
+  let a = Math.floor(Math.random() * 100);
+  let b = Math.floor(Math.random() * 100);
+  let c = Math.floor(Math.random() * 100);
+  body.style.color = `#${a}${b}${c}`;
+});
+
+// drag / drop needs to happen
+const dragImg = document.querySelector("section img");
+dragImg.addEventListener("dragstart", () => {
+    dragImg.style.opacity = 0.5;
+    dragImg.style.border = "2px dashed blue";
+  });
+
 // load
-// focus
+window.addEventListener('load', () => {
+    alert('Page was fully loaded.');
+});
+
 // resize
+window.addEventListener('resize', () => console.log("Window was resized."));
+
+// focus
 // scroll
 // select
 
 // dblclick
 const header = document.querySelector("header");
-header.addEventListener('dblclick', () => {
-    let a = Math.floor(Math.random() * 100);
-    let b = "red";
-    header.style.backgroundColor = `#DD${a}${a}`;
+header.addEventListener("dblclick", () => {
+  let a = Math.floor(Math.random() * 100);
+  header.style.backgroundColor = `#DD${a}${a}`;
 });
