dfc634b6b12f43c55ade62e552ab0881cbc745de
vebradev <justinas@vebra.dev>
2019-05-15 22:40:30 +0300

    Add wheel counter to footer.

--- js/index.js
+++ js/index.js
@@ -2,23 +2,42 @@
 
 const header = document.querySelector("header");
 const container = document.querySelector(".home");
+const footer = document.querySelector(".footer");
 
-// mouseover
+// mouseover header
 
-header.addEventListener('mouseover', () => { 
-    header.classList.add("shadow");
-    container.classList.add("opacity");
+header.addEventListener("mouseover", () => {
+  header.classList.add("shadow");
+  container.classList.add("opacity");
 });
 
-header.addEventListener('mouseout', () => { 
-    header.classList.remove("shadow");
-    container.classList.remove("opacity");
+header.addEventListener("mouseout", () => {
+  header.classList.remove("shadow");
+  container.classList.remove("opacity");
 });
 
-// keydown
+// keydown (l)
 
+document.addEventListener("keydown", event => {
+  const loginKey = 76;
+  if (event.which === loginKey) {
+    header.insertAdjacentHTML("beforebegin", "<div class='login-screen'>Foo</div>");
+  }
+});
 
 // wheel
+
+let wheelTurns = 0;
+
+window.addEventListener("wheel", () => {
+  wheelTurns++;
+  counter.textContent = `Wheel moved ${wheelTurns} times.`
+});
+
+const counter = document.createElement("span");
+counter.className = "counter";
+footer.appendChild(counter);
+
 // drag / drop
 // load
 // focus
